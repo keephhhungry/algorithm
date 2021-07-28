af9acd8a53ff8b0afceb4b749236598b2d649158
keephhhungry <54006022@qq.com>
2021-07-28 23:16:08 +0800

    提交7月28号的一题题目

---
+++ src/y2021/m07/d28/MissingNumber.java
@@ -0,0 +1,71 @@
+package y2021.m07.d28;
+
+import com.sun.media.sound.EmergencySoundbank;
+
+/**
+ * @author ： cxyxh
+ * @date : 7/28/2021 10:58 PM
+ * @describetion :leetcode编号268. 丢失的数字
+ * 给定一个包含 [0, n] 中 n 个数的数组 nums ，找出 [0, n] 这个范围内没有出现在数组中的那个数。
+ * <p>
+ * 进阶：
+ * 你能否实现线性时间复杂度、仅使用额外常数空间的算法解决此问题?
+ * <p>
+ * 示例 1：
+ * 输入：nums = [3,0,1]
+ * 输出：2
+ * 解释：n = 3，因为有 3 个数字，所以所有的数字都在范围 [0,3] 内。2 是丢失的数字，因为它没有出现在 nums 中。
+ * <p>
+ * 示例 2：
+ * 输入：nums = [0,1]
+ * 输出：2
+ * 解释：n = 2，因为有 2 个数字，所以所有的数字都在范围 [0,2] 内。2 是丢失的数字，因为它没有出现在 nums 中。
+ * <p>
+ * 示例 3：
+ * 输入：nums = [9,6,4,2,3,5,7,0,1]
+ * 输出：8
+ * 解释：n = 9，因为有 9 个数字，所以所有的数字都在范围 [0,9] 内。8 是丢失的数字，因为它没有出现在 nums 中。
+ * <p>
+ * 示例 4：
+ * 输入：nums = [0]
+ * 输出：1
+ * 解释：n = 1，因为有 1 个数字，所以所有的数字都在范围 [0,1] 内。1 是丢失的数字，因为它没有出现在 nums 中。
+ * <p>
+ * 提示：
+ * <p>
+ * n == nums.length
+ * 1 <= n <= 104
+ * 0 <= nums[i] <= n
+ * nums 中的所有数字都 独一无二
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/missing-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MissingNumber {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 1, 3};
+        int ans = missingNumber(nums);
+        System.out.println(ans);
+    }
+
+    /**
+     * 位运算
+     * 例： (0 ^ 1 ^ 3)  ^ (0 ^ 1 ^ 2)
+     * 上述为 数组的元素 ^ 数组的角标
+     * 多 ^ 数组的长度 变成 (0 ^ 1 ^ 3)  ^ (0 ^ 1 ^ 2) ^ 3 既可以得到答案
+     * 时间复杂度O(n)
+     * 空间复杂度O(1)
+     *
+     * @param nums
+     * @return
+     */
+    public static int missingNumber(int[] nums) {
+        int ans = nums.length;
+        for (int i = 0; i < ans; i++) {
+            ans = ans ^ i ^ nums[i];
+        }
+        return ans;
+    }
+}
