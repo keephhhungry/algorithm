c94106b40bdbc517ce8464e03b181ca9be5fed1c
keephhhungry <54006022@qq.com>
2021-08-05 23:02:15 +0800

    提交8月5号的一题题目

---
+++ src/y2021/m08/d05/InvertTree.java
@@ -0,0 +1,88 @@
+package y2021.m08.d05;
+
+/**
+ * @author ： cxyxh
+ * @date : 8/5/2021 10:36 PM
+ * @describetion :226. 翻转二叉树
+ * 翻转一棵二叉树。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：
+ * <p>
+ * 4
+ * /   \
+ * 2     7
+ * / \   / \
+ * 1   3 6   9
+ * 输出：
+ * <p>
+ * 4
+ * /   \
+ * 7     2
+ * / \   / \
+ * 9   6 3   1
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/invert-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/invert-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class InvertTree {
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+    }
+
+    /**
+     * 时间复杂度O(n)
+     * 空间复杂度O(n)
+     *
+     * @param root
+     * @return
+     */
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+}
