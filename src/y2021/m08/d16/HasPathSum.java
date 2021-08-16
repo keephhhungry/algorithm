e179d2c82a417cd16f698f57bd61b5142d15a873
keephhhungry <54006022@qq.com>
2021-08-16 23:13:32 +0800

    提交8月16号的每日一题

---
+++ src/y2021/m08/d16/HasPathSum.java
@@ -0,0 +1,81 @@
+package y2021.m08.d16;
+
+/**
+ * @author ： cxyxh
+ * @date : 8/16/2021 10:50 PM
+ * @describetion :112. 路径总和
+ * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。
+ * 叶子节点 是指没有子节点的节点。
+ * <p>
+ * 示例 1：
+ * 输入：root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+ * 输出：true
+ * <p>
+ * 示例 2：
+ * 输入：root = [1,2,3], targetSum = 5
+ * 输出：false
+ * <p>
+ * 示例 3：
+ * 输入：root = [1,2], targetSum = 0
+ * 输出：false
+ *  
+ * 提示：
+ * <p>
+ * 树中节点的数目在范围 [0, 5000] 内
+ * -1000 <= Node.val <= 1000
+ * -1000 <= targetSum <= 1000
+ * 通过次数246,427提交次数468,842
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/path-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class HasPathSum {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right = new TreeNode(8);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        boolean res = hasPathSum(root, 22);
+        System.out.println(res);
+    }
+
+    /**
+     * @param root 时间复杂度O(n) 节点数
+     * @param sum  空间复杂度O(h) 树的高度
+     * @return
+     */
+    public static boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return sum == root.val;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
