29d840e12f8ff9f81244f4ed01aad48ccd71ba0d
keephhhungry <54006022@qq.com>
2021-08-20 22:21:05 +0800

    提交8月20号的每日一题

---
+++ src/y2021/m08/d20/SumOfLeftLeaves.java
@@ -0,0 +1,75 @@
+package y2021.m08.d20;
+
+/**
+ * @author ： cxyxh
+ * @date : 8/20/2021 10:07 PM
+ * @describetion :404. 左叶子之和
+ * 计算给定二叉树的所有左叶子之和。
+ * <p>
+ * 示例：
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * <p>
+ * 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ *  
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sum-of-left-leaves
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SumOfLeftLeaves {
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        int res = sumOfLeftLeaves(root);
+        System.out.println(res);
+    }
+
+    /**
+     * 递归
+     * @param root
+     * @return
+     */
+    public static int sumOfLeftLeaves(TreeNode root) {
+        int sum = 0;
+        if (root == null) {
+            return 0;
+        }
+        if (root.left != null) {
+            if (root.left.left == null && root.left.right == null) {
+                sum += root.left.val;
+            }
+        }
+        sum += sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+        return sum;
+    }
+
+}
