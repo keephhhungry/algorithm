eba313565f5fdf5a148ffea5b920f5063e1428a7
keephhhungry <54006022@qq.com>
2021-08-23 21:16:35 +0800

    提交8月23号的每日一题

---
+++ src/y2021/m08/d23/IsMonotonic.java
@@ -0,0 +1,69 @@
+package y2021.m08.d23;
+
+/**
+ * @author ： cxyxh
+ * @date : 8/23/2021 8:48 PM
+ * @describetion :896. 单调数列
+ * 如果数组是单调递增或单调递减的，那么它是单调的。
+ * 如果对于所有 i <= j，A[i] <= A[j]，那么数组 A 是单调递增的。 如果对于所有 i <= j，A[i]> = A[j]，那么数组 A 是单调递减的。
+ * 当给定的数组 A 是单调数组时返回 true，否则返回 false。
+ * <p>
+ * 示例 1：
+ * 输入：[1,2,2,3]
+ * 输出：true
+ * <p>
+ * 示例 2：
+ * 输入：[6,5,4,4]
+ * 输出：true
+ * <p>
+ * 示例 3：
+ * 输入：[1,3,2]
+ * 输出：false
+ * <p>
+ * 示例 4：
+ * 输入：[1,2,4,5]
+ * 输出：true
+ * <p>
+ * 示例 5：
+ * 输入：[1,1,1]
+ * 输出：true
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/monotonic-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IsMonotonic {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 4, 5};
+        boolean res = isMonotonic(nums);
+        System.out.println(res);
+    }
+
+    /**
+     * 时间复杂度O(n)
+     * 空间复杂度O(1)
+     *
+     * @param nums
+     * @return
+     */
+    public static boolean isMonotonic(int[] nums) {
+        boolean inc = true, dec = true;
+        int n = nums.length;
+        for (int i = 0; i < n - 1; ++i) {
+            if (nums[i] > nums[i + 1]) {
+                inc = false;
+                if (dec == false) {
+                    return false;
+                }
+            }
+            if (nums[i] < nums[i + 1]) {
+                dec = false;
+                if (inc == false) {
+                    return false;
+                }
+            }
+        }
+        return inc || dec;
+    }
+}
