5bf8497e779e4f27a0fee7a339be7241f372dbb5
keephhhungry <54006022@qq.com>
2021-08-26 23:52:08 +0800

    提交8月26号的每日一题

---
+++ src/y2021/m08/d26/ReverseWords.java
@@ -0,0 +1,52 @@
+package y2021.m08.d26;
+
+/**
+ * @author ： cxyxh
+ * @date : 8/26/2021 11:44 PM
+ * @describetion :557. 反转字符串中的单词 III
+ * 给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入："Let's take LeetCode contest"
+ * 输出："s'teL ekat edoCteeL tsetnoc"
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-words-in-a-string-iii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ReverseWords {
+
+    public static void main(String[] args) {
+        String s = "Let's take LeetCode contest";
+        String res = reverseWords(s);
+        System.out.println(res);
+    }
+
+    /**
+     * 时间复杂度O(n)
+     * 空间复杂度O(n)
+     *
+     * @param s
+     * @return
+     */
+    public static String reverseWords(String s) {
+        StringBuffer ret = new StringBuffer();
+        int length = s.length();
+        int i = 0;
+        while (i < length) {
+            int start = i;
+            while (i < length && s.charAt(i) != ' ') {
+                i++;
+            }
+            for (int p = start; p < i; p++) {
+                ret.append(s.charAt(start + i - 1 - p));
+            }
+            while (i < length && s.charAt(i) == ' ') {
+                i++;
+                ret.append(' ');
+            }
+        }
+        return ret.toString();
+    }
+}
