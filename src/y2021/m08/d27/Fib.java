1404f7d9bc15fd6cf5619185cbe2c32396d2f3d8
keephhhungry <54006022@qq.com>
2021-08-27 23:45:44 +0800

    提交8月27号的每日一题

---
+++ src/y2021/m08/d27/Fib.java
@@ -0,0 +1,55 @@
+package y2021.m08.d27;
+
+/**
+ * @author ： cxyxh
+ * @date : 8/27/2021 11:17 PM
+ * @describetion :509. 斐波那契数
+ * 斐波那契数，通常用 F(n) 表示，形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ * <p>
+ * F(0) = 0，F(1) = 1
+ * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+ * 给你 n ，请计算 F(n) 。
+ * <p>
+ * 示例 1：
+ * 输入：2
+ * 输出：1
+ * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+ * <p>
+ * 示例 2：
+ * 输入：3
+ * 输出：2
+ * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2
+ * <p>
+ * 示例 3：
+ * 输入：4
+ * 输出：3
+ * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fibonacci-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Fib {
+
+    public static void main(String[] args) {
+        int res = fib(3);
+        System.out.println(res);
+    }
+
+    /**
+     * @param n
+     * @return
+     */
+    public static int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int p = 0, q = 0, r = 1;
+        for (int i = 2; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
+}
