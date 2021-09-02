d32ed2dacbca0ba7dc9c97ea93346efe19f88101
keephhhungry <54006022@qq.com>
2021-09-02 23:47:08 +0800

    提交9月2号的每日一题

---
+++ src/y2021/m09/d02/RepeatedSubstringPattern.java
@@ -0,0 +1,54 @@
+package y2021.m09.d02;
+
+/**
+ * @author ： cxyxh
+ * @date : 9/2/2021 11:44 PM
+ * @describetion :459. 重复的子字符串
+ * 给定一个非空的字符串，判断它是否可以由它的一个子串重复多次构成。给定的字符串只含有小写英文字母，并且长度不超过10000。
+ * <p>
+ * 示例 1:
+ * 输入: "abab"
+ * 输出: True
+ * 解释: 可由子字符串 "ab" 重复两次构成。
+ * <p>
+ * 示例 2:
+ * 输入: "aba"
+ * 输出: False
+ * <p>
+ * 示例 3:
+ * 输入: "abcabcabcabc"
+ * 输出: True
+ * <p>
+ * 解释: 可由子字符串 "abc" 重复四次构成。 (或者子字符串 "abcabc" 重复两次构成。)
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/repeated-substring-pattern
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class RepeatedSubstringPattern {
+
+    public static void main(String[] args) {
+        String s = "abab";
+        boolean res = repeatedSubstringPattern(s);
+        System.out.println(res);
+    }
+
+    public static boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for (int i = 1; i * 2 <= n; ++i) {
+            if (n % i == 0) {
+                boolean match = true;
+                for (int j = i; j < n; ++j) {
+                    if (s.charAt(j) != s.charAt(j - i)) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
