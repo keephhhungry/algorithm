0255c47323fa7eaf59b22865d908c78f20fb3fa2
keephhhungry <54006022@qq.com>
2021-10-03 22:07:21 +0800

    提交10月3号的每日一题

---
+++ src/y2021/m10/d03/IsSubtree.java
@@ -0,0 +1,84 @@
+package y2021.m10.d03;
+
+/**
+ * @author ： cxyxh
+ * @date : 10/3/2021 9:57 PM
+ * @describetion :572. 另一棵树的子树
+ * 给你两棵二叉树 root 和 subRoot 。检验 root 中是否包含和 subRoot 具有相同结构和节点值的子树。如果存在，返回 true ；否则，返回 false 。
+ * <p>
+ * 二叉树 tree 的一棵子树包括 tree 的某个节点和这个节点的所有后代节点。tree 也可以看做它自身的一棵子树。
+ * <p>
+ * 提示：
+ * <p>
+ * root 树上的节点数量范围是 [1, 2000]
+ * subRoot 树上的节点数量范围是 [1, 1000]
+ * -104 <= root.val <= 104
+ * -104 <= subRoot.val <= 104
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/subtree-of-another-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IsSubtree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(4);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(4);
+
+        boolean res = isSubtree(root, root.left);
+        System.out.println(res);
+
+    }
+
+    /**
+     * 深度优先搜索暴力匹配
+     *
+     * @param root
+     * @param subRoot
+     * @return
+     */
+    public static boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        return dfs(root, subRoot);
+    }
+
+
+    public static boolean dfs(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+        return check(s, t) || dfs(s.left, t) || dfs(s.right, t);
+    }
+
+    public static boolean check(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null || s.val != t.val) {
+            return false;
+        }
+        return check(s.left, t.left) && check(s.right, t.right);
+    }
+
+}
